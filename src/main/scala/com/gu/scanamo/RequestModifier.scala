4b0da27004679443be4ca705cce9e2e885d2bb48
Henrik Gustafsson <henrik.gustafsson@ticketmaster.co.uk>
2016-12-05 13:00:28 +0000

    Add WithConsistentRead request modifier

--- src/main/scala/com/gu/scanamo/RequestModifier.scala
+++ src/main/scala/com/gu/scanamo/RequestModifier.scala
@@ -1,5 +1,7 @@
 package com.gu.scanamo
 
+import com.amazonaws.services.dynamodbv2.model.GetItemRequest
+
 trait RequestModifier[T] {
   def apply(t: T): T
 }
@@ -7,5 +9,12 @@ trait RequestModifier[T] {
 object RequestModifier {
   def nullModifier[T] = new RequestModifier[T] {
     override def apply(t: T): T = t
+  }
+}
+
+object GetItem {
+  type Modifier = RequestModifier[GetItemRequest]
+  object WithConsistentRead extends Modifier {
+    override def apply(t: GetItemRequest): GetItemRequest = t.withConsistentRead(true)
   }
 }
